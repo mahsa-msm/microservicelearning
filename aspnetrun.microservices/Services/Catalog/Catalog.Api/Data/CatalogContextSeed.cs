90fc9c8afe88bee513e627bba4433b9db322a620
mahsaamousavi <mahsaa.mousavi77@gmail.com>
2022-02-07 17:48:29 +0330

    added db contxt for mongo and added product entity

---
+++ aspnetrun.microservices/Services/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> productCollection)
+        {
+            bool existProduct = productCollection.Find(c => true).Any();
+            if(!existProduct)
+            {
+                //productCollection.InsertMany();
+            }
+        }
+    }
+}
