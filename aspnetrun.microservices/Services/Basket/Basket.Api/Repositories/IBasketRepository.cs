7ecb8ec62dc937d0038ef6ad0461dd2ebbb8d331
mahsa mousavi <mahsaa.mousavi77@gmail.com>
2022-02-11 12:30:14 +0330

    added basket api

---
+++ aspnetrun.microservices/Services/Basket/Basket.Api/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.Api.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.Api.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart shoppingCart);
+        Task DeleteBasket(string userName);
+    }
+}
